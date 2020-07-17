b492cc6fdcd71850080ac26fa061c463735d6acc
Maysoon Tabali <mtab03@me.com>
2020-07-17 20:38:30 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -11,7 +11,7 @@ def using_unshift(array, string)
  end 
  
  def using_pop(array)
-  @array = ["Africa", "Asia", "Europe", "North America", "South America", "Oceania", "Antarctica"]
+  array = ["Africa", "Asia", "Europe", "North America", "South America", "Oceania", "Antarctica"]
   Antarctica array.pop
  # p array 
  # p Antarctica
