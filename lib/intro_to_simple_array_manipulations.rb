ce805dbe814afd691b822fe1d2441da5ec802f5d
Maysoon Tabali <mtab03@me.com>
2020-07-17 17:51:49 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,32 @@
+def using_push(array, string)
+array = ["red", "orange", "yellow", "green", "blue", "indigo"]
+string = "violet"
+array.push("violet")
+end 
+
+def using_unshift(array, string)
+   bouroughs_in_nyc = ["Brooklyn", "Queens", "Manhattan", "Bronx"]
+   string = "Staten Island"
+   array.unshift("Staten Island")
+ end 
+ 
+ def using_pop(array)
+  array = ["Africa", "Asia", "Europe", "North America", "South America", "Oceania", "Antarctica"]
+  Antarctica array.pop
+  p array 
+  p Antarctica
+end
+
+def pop_with_args(dog_breeds)
+ dog_breeds = ["Great Dane", "Golden Retriever", "Chihuahua", "Shiba Inu"]
+ #small_dogs = dog_breeds
+ dog_breeds.pop(2)
+ end
+ 
+ #puts dog_breeds.pop(2)
+ #puts dog_breeds
+ #small_dogs.pop[2], [3]
+ #small_dogs = pop_with_args(@dog_breeds
+ #end
+  
+ 
